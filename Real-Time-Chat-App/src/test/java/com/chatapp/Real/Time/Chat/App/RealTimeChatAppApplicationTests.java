c1c1745449c71e063a7b6f5f4430513817faf4c9
[Niggle Nandu] <nigglenandu@gmail.com>
2025-01-07 22:33:49 +0545

    project

---
+++ Real-Time-Chat-App/src/test/java/com/chatapp/Real/Time/Chat/App/RealTimeChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.Real.Time.Chat.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
